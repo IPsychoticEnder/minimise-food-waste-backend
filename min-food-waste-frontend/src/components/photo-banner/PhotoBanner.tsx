9d02f2cf37d14841427dcc5e7cad5da6dda191c1
Barn Owl Studios <95099604+IPsychoticEnder@users.noreply.github.com>
2026-02-23 09:16:46 +0100

    Added ideation doc + a photo banner

---
+++ min-food-waste-frontend/src/components/photo-banner/PhotoBanner.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function PhotoBanner({}: Props) {
+  return (
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ea nostrum id voluptas est in itaque et accusantium hic, eos porro eligendi quas ratione ab impedit nihil repudiandae possimus quis!</div>
+  )
+}
+
+export default PhotoBanner
---
+++ min-food-waste-frontend/src/pages/home-page/home-page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import PhotoBanner from '../../components/photo-banner/PhotoBanner'
+
+type Props = {}
+
+function HomePage({}: Props): React.ReactNode {
+  return (
+    <PhotoBanner/>
+  )
+}
+
+export default HomePage
